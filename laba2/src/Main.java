b0dc0400ff27cfbc159c541b64e65319d39a4136
Evgzek <zekamusijhcuk@gmail.com>
2021-06-06 15:30:02 +1000

    главная функция

---
+++ laba2/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Введите максимальную вершину: ");
+    int maxStep = in.nextInt();
+
+    System.out.print("Введите максимальный перепад трех ближайших вершин: ");
+    int maxDiff = in.nextInt();
+
+    System.out.print("Введите длину маршрута: ");
+    int routeLength = in.nextInt();
+
+    Roc test = new Roc(maxStep,maxDiff,routeLength);
+
+    test.PrintGraph();
+    System.out.println(test.toString());
+
+
+}
+}
